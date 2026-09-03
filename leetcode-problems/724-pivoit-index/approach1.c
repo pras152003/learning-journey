2ceaea6dffbf460f1b16e574ec74f46c613d7b1f
PrashantKumar Teggi <pteggi9@gmail.com>
2026-09-03 13:02:38 +0530

    Add pivotIndex function to find pivot index
    Implement pivot index calculation with left and right sums.

--- leetcode-problems/724-pivoit-index/approach1.c
+++ leetcode-problems/724-pivoit-index/approach1.c
@@ -1,1 +1,22 @@
 
+int pivotIndex(int* nums, int numsSize) {
+    int leftsum=0;
+    int rightsum=0;
+    int i=0;
+    int res=0;
+    while(i<numsSize){
+        rightsum=0;
+        for(int j=i+1;j<numsSize;j++){
+            rightsum+=nums[j];
+        }
+        if(leftsum==rightsum){
+            return i;
+            break;
+        }
+        else{
+            leftsum+=nums[i];
+            i++;
+        }
+    }
+    return -1;
+ }
