d4abcb0ef88994654ba1efe2857a67a3009a581b
PrashantKumar Teggi <pteggi9@gmail.com>
2026-09-03 13:04:38 +0530

    Implement pivot index function in C

--- leetcode-problems/724-pivoit-index/approach2_better.c
+++ leetcode-problems/724-pivoit-index/approach2_better.c
@@ -1,1 +1,26 @@
 
+int pivotIndex(int* nums, int numsSize)
+{
+    int leftsum = 0;
+    int rightsum = 0;
+
+    for(int j = 0; j < numsSize; j++)
+    {
+        rightsum += nums[j];
+    }
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(leftsum == rightsum - nums[i])
+        {
+            return i;
+        }
+        else
+        {
+            leftsum += nums[i];
+            rightsum -= nums[i];
+        }
+    }
+
+    return -1;
+}
