8ea4385c2398ab5dd600a4cb3fbc5484fbe30dff
PrashantKumar Teggi <pteggi9@gmail.com>
2026-09-04 13:26:46 +0530

    Implement reverseString function to reverse a string

--- leetcode-problems/344-reverse-string/solutin1.c
+++ leetcode-problems/344-reverse-string/solutin1.c
@@ -1,1 +1,13 @@
 
+void reverseString(char* s, int sSize) {
+    char temp;
+    int i=0;
+    int j=sSize-1;
+    while(i<sSize/2){
+        temp=s[j];
+        s[j]=s[i];
+        s[i]=temp;
+        i++;
+        j--;
+    }
+}
